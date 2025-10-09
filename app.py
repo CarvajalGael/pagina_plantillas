79c2f8a4cf809f08bfc3f404c298dbff09f53046
CarvajalGael <23308060610312@cetis61.edu.mx>
2025-10-09 10:45:00 -0500

    actualizacion

--- app.py
+++ app.py
@@ -3,6 +3,28 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def base():
-    return render_template("base.html")
+def index():
+    return render_template("index.html")
+
+@app.route("/animales")
+def animales():
+    return render_template("animales.html")
+
+@app.route("/autos")
+def autos():
+    return render_template("autos.html")
+
+@app.route("/maravillas")
+def maravillas():
+    return render_template("maravillas.html")
+
+@app.route("/acercas")
+def acercas():
+    return render_template("acercas.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
 
